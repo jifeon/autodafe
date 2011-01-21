facebcea8c2a6781a6c78ad380148ccf7e58a0d0
Balakirev Andrey <balakirev.andrey@gmail.com>
2011-01-21 16:33:09 +0300

    rm module color

---
+++ demos/sample_config.js
@@ -0,0 +1,45 @@
+  var config = module.exports = {
+    base_dir        : __dirname + '/../',
+    name            : 'Application name',
+
+    default_controller  : 'site',
+
+    router : {
+
+      rules     : {
+        'abbreviation' : 'controller_name.action_name'
+      }
+    },
+
+    preload_components : [ 'log_router' ],
+
+    components : {
+
+      web_sockets_server  : true,
+      user                : true,
+
+      db                  : {
+        type : 'mysql',
+        user : 'root',
+        pass : '',
+        base : 'test',
+        host : 'localhost',
+        dbslayer : {
+          host : 'localhost',
+          port : 9090
+        }
+      },
+
+      log_router          : {
+
+        routes : {
+          console : {
+            levels : [ 'trace', 'info', 'warning', 'error' ]
+          },
+          file : {
+            levels : [ 'trace', 'info', 'warning', 'error' ]
+          }
+        }
+      }
+    }
+  };
