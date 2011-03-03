8b08bb2ca920cd2cd681c7978337e63989d378e1
Balakirev Vladimir <balakirev.vladimir@rambler.ru>
2011-03-03 16:57:15 +0300

    native mysql connector

---
+++ framework/db/mysql/mysql_db_connection2.js
@@ -0,0 +1,100 @@
+var DB        = require('../db_connection');
+var sys       = require('sys');
+var mysql = require('./mysql-libmysqlclient');
+
+var MysqlDBConnection = module.exports = function( config ) {
+  this._init( config );
+};
+
+
+require('sys').inherits( MysqlDBConnection, DB );
+
+
+MysqlDBConnection.prototype._init = function( config ) {
+  DB.prototype._init.call( this, config );
+  var self = this;
+
+  this.initialized  = false;
+
+ // this.connect();
+  this.conn = mysql.createConnectionSync();
+  this.conn.connectSync(self.host, self.user, self.pass, self.base);
+  if (!this.conn.connectedSync()) {
+  	sys.puts("Connection error " + this.conn.connectErrno + ": " + this.conn.connectError);
+  	process.exit(1);
+	} 
+  this.initialized = true;
+  this.query = this.__query;
+};
+
+MysqlDBConnection.prototype.query = function ( sql, callback, errback ) {
+  var self = this;
+
+  var emitter = new process.EventEmitter;
+
+  this.on( 'initialized', function() {
+    self.__query( sql, callback, errback, emitter );
+  } );
+
+  return emitter;
+};
+
+
+MysqlDBConnection.prototype.__query = function ( sql, callback, errback, emitter ) {
+  var emitter = emitter || new process.EventEmitter;
+  
+  if ( typeof sql != "string" || !sql.length ) {
+    this.app.log( 'Bad sql "%s"'.format( sql ), 'error', 'MysqlDBConnection' );
+    return emitter;
+  }
+  errback  = errback  || this.standard_errback;
+  callback = callback || function(){};
+
+  this.app.log( 'Quering sql: ' + sql, 'trace', 'MysqlDBConnection' );
+  var db = this;
+  this.conn.query( sql, function( err, res) {
+  	  if (err) {
+  	      errback( 'mysql error: ' + err );
+  	  }
+  	  callback.call(db, res);
+  });
+  return emitter;
+};
+
+
+MysqlDBConnection.prototype.standard_errback = function ( message ) {
+  this.app.log( message, 'error', 'MysqlDBConnection' );
+};
+
+
+MysqlDBConnection.prototype.fetch_array = function ( res, callback ) {
+  //	if ( (res.fieldCountGetter() > 0) || (typeof callback != 'function') ) return false;
+  if ( (this.conn.affectedRowsSync() == 0) || (typeof callback != 'function') ) return false;
+
+	while (row = res.fetchArraySync()) {
+    	if ( callback.call( this, row ) === false ) break;
+	}
+};
+
+
+MysqlDBConnection.prototype.fetch_args = function ( res, callback ) {
+//  	if ( (res.fieldCountGetter() > 0) || (typeof callback != 'function') ) return false;
+  if ( (this.conn.affectedRowsSync() == 0) || (typeof callback != 'function') ) return false;
+
+	while (row = res.fetchArraySync()) {
+    	if ( callback.apply( this, row ) === false ) break;
+	}
+};
+
+
+MysqlDBConnection.prototype.fetch_obj = function ( res, callback ) {
+  if ( (this.conn.affectedRowsSync() == 0) || (typeof callback != 'function') ) return false;
+
+	while (row = res.fetchArraySync()) {
+    var obj = {};
+	  for(var i = 0, ln = row.length; i < ln; i++){
+	    obj[ res.fetchFieldDirectSync(i).name ] = row[ i ];
+	  }
+	  if ( callback.call( this, obj ) === false ) break;
+	}
+};
---
+++ tests/unit/framework/db/ar/active_record_tests.js
@@ -0,0 +1,151 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jifeon
+ * Date: 21.02.11
+ * Time: 19:53
+ * To change this template use File | Settings | File Templates.
+ */
+
+exports.test = function(app) {
+
+  var vows  = app.tests.vows;
+  var assert = require('assert');
+
+  var ActiveRecord = require('db/ar/active_record');
+
+  var Post = module.exports = function(params) {
+    ActiveRecord.prototype._init.call(this, params);
+  };
+
+
+  Post.get_table_name = function () {
+    return 'posts';
+  };
+
+  Post.model = function() {
+    return ActiveRecord.model(this);
+  };
+
+  require('sys').inherits(Post, ActiveRecord);
+
+var commands = {
+  find: function ( model, func, cond, attr, pk, sql)
+  {
+    var command_emitter = new process.EventEmitter;
+
+  switch( func ) {
+    case 'find' :
+      model.find( cond, attr ).on( 'complete', function( res ) {
+          command_emitter.emit( 'success', res );
+          } );
+    break;
+
+    case 'find_all' :
+      model.find_all( cond, attr ).on( 'complete', function( res ) {
+          command_emitter.emit( 'success', res );
+          } );
+    break;
+
+    case 'find_by_pk' :
+      model.find_by_pk( pk, cond, attr ).on( 'complete', function( res ) {
+          command_emitter.emit( 'success', res );
+          } );
+    break;
+  }
+
+  return command_emitter;
+  }
+}
+
+  vows.describe('active_record_tests').addBatch({
+    'ActiveRecord test' : {
+      topic : new ( Post ),
+      'db test'         : function( topic ){
+        assert.equal( topic.get_db_connection(), app.db );
+      },
+      'table test'      : function( topic ){
+        assert.equal( topic.table, 'posts' );
+      },
+      'set_attributes'  : function( topic ){
+        assert.isUndefined( topic.id );
+        topic.set_attributes( { 'id' : 3, 'title' : 'test title' } );
+        assert.isUndefined( topic.id );
+        assert.equal( topic.title, 'test title' );
+      },
+      'primary key'     : function(topic) {
+        assert.equal(topic.primary_key(), 'id');
+      },
+      'table_schema'    : function( topic ){
+//        var ts = require('db/db_table_schema');
+//        assert.equal( topic.get_table_schema(), ts );
+      },
+      'model test'      : {
+        topic : function( topic ){
+          return topic.get_model();
+        },
+        'instance test'   : function( model ) {
+          assert.instanceOf( model, Post );
+        },
+        'functions tests' : {
+          'find command'  : {
+            topic         : function( model ){ return commands.find( model, 'find', '', {}, 0, '' ) },
+          'tests "id"'    : function( res ) {
+            assert.instanceOf( res, Post );
+            assert.equal(res.id, 1);
+          }
+        },
+          'find with condition command'  : {
+            topic         : function( model ){ return commands.find( model, 'find', 'id=:id', { id : 5 }, 0, '' ) },
+            'tests "id"'  : function( res ) {
+              assert.instanceOf( res, Post );
+              assert.equal( res.id, 5 );
+            }
+          },
+          'find without result'  : {
+            topic         : function( model ){ return commands.find( model, 'find', 'id=6', {}, 0, '' ) },
+            'tests "id"'  : function( res ) {
+              assert.isNull( res );
+            }
+          },
+          'find_all command' : {
+            topic         : function( model ){ return commands.find( model, 'find_all', '', {}, 0, '' ) },
+            'lenght,instance,id' : function( res ){
+              assert.length( res, 5 );
+              assert.instanceOf( res[3], Post );
+              assert.equal( res[3].id, 4 );
+            }
+          },
+          'find_all with condition ' : {
+            topic         : function( model ){
+              return commands.find( model, 'find_all', { limit : 3, offset : 1 }, {}, 0, '')
+            },
+            'lenght,instance,id' : function( res ){
+              assert.length( res, 3 );
+              assert.instanceOf( res[2], Post );
+              assert.equal( res[2].id, 4 );
+            }
+          },
+          'find_all without result' : {
+            topic         : function( model ){
+              return commands.find( model, 'find_all', 'id = 6', {}, 0, '' )
+            },
+            'empty result?' : function( res ){
+              assert.length( res, 0 );
+            }
+          },
+          'find_by_pk' : {
+            topic         : function( model ){
+              return commands.find( model, 'find__by_pk', '', {}, 2, '' )
+            },
+            'test "id"' : function( res ){
+              assert.equal( res.id, 2 );
+            }
+          }
+        }
+      }
+    }
+  });
+  var t = new Post();
+ // console.log(t.get_meta_data());
+  //t.get_model().find_by_pk(2,'',{});
+}
