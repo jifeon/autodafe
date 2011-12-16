1486afd9314da2d00295e65bbe8a33527ecaf0a3
Andrey Balakirev <balakirev.andrey@gmail.com>
2011-12-16 18:48:02 +0400

    новый inheriting_proxy_handler

--- framework/lib/proxy_handlers/inheriting_proxy_handler.js
+++ framework/lib/proxy_handlers/inheriting_proxy_handler.js
@@ -12,64 +12,90 @@ require('util').inherits( InheritingProxyHandler, ProxyHandler );
 InheritingProxyHandler.prototype._init = function ( params ) {
   ProxyHandler.prototype._init.call( this, params );
 
-  this.context = params.context;
-};
+  this.context         = params.context;
+  this.stack           = {};
 
-
-InheritingProxyHandler.prototype.get = function ( receiver, name ) {
-  var self = this;
-
-  return typeof this.target[name] != 'function'
-    ? this.target[name]
-    : function() {
-
-        var super_prototype = self.target;
-        var caller          = arguments.callee.caller;
-        var calling_chain   = caller.chain || [ caller ];
-
-        while (
-          !super_prototype.hasOwnProperty( name ) ||
-          calling_chain.indexOf( super_prototype[ name ] ) != -1
-        ) {
-          super_prototype = Object.getPrototypeOf( super_prototype );
-          if ( super_prototype === null )
-            throw new TypeError( name + ' is not a function' );
-        }
-
-        var method = super_prototype[ name ];
-        calling_chain.push( method );
-        method.chain = calling_chain;
-        try {
-          var result = method.apply( self.context, arguments );
-        }
-        catch( e ) {
-          delete method.chain;
-          throw e;
-        }
-        delete method.chain;
-
-        return result;
-      };
+  this.super_prototype = this.target;
+  this.calling         = false;
 };
 
 
 //InheritingProxyHandler.prototype.get = function ( receiver, name ) {
 //  var self = this;
 //
-//  if ( typeof this.target[name] != 'function' ) return this.target[name];
+//  return typeof this.target[name] != 'function'
+//    ? this.target[name]
+//    : function() {
 //
-//  var super_prototype = self.target;
-//  while ( !super_prototype.hasOwnProperty( name ) ) {
-//    super_prototype = Object.getPrototypeOf( super_prototype );
-//    if ( super_prototype === null )
-//      throw new TypeError( name + ' is not a function' );
-//  }
+//        var super_prototype = self.target;
+//        var caller          = arguments.callee.caller;
+//        var calling_chain   = caller.chain || [ caller ];
 //
-//  var super_pointer_handler = new ProxyHandler({ target : self.context });
-//  super_pointer_handler.get = function( r, name ){
-//    if ( name == 'super_' ) return super_prototype.super_;
-//    return this.__proto__.get.call( this, r, name );
-//  }
+//        while (
+//          !super_prototype.hasOwnProperty( name ) ||
+//          calling_chain.indexOf( super_prototype[ name ] ) != -1
+//        ) {
+//          super_prototype = Object.getPrototypeOf( super_prototype );
+//          if ( super_prototype === null )
+//            throw new TypeError( name + ' is not a function' );
+//        }
 //
-//  return super_prototype[ name ].bind( super_pointer_handler.get_proxy() );
+//        var method = super_prototype[ name ];
+//        calling_chain.push( method );
+//        method.chain = calling_chain;
+//        try {
+//          var result = method.apply( self.context, arguments );
+//        }
+//        catch( e ) {
+//          delete method.chain;
+//          throw e;
+//        }
+//        delete method.chain;
+//
+//        return result;
+//      };
 //};
+
+
+InheritingProxyHandler.prototype.get = function ( receiver, name ) {
+  if ( typeof this.target[name] != 'function' ) return this.target[name];
+
+  var root = false;
+  if ( !this.stack[ name ] ){
+    this.stack[ name ] = this.target;
+    root               = true;
+  }
+
+  var super_prototype = this.get_prototype_with_method( this.stack[ name ], name );
+  if ( root ) super_prototype = this.get_prototype_with_method( Object.getPrototypeOf( super_prototype ), name );
+
+  var context = this.context;
+  var self    = this;
+  this.stack[ name ] = Object.getPrototypeOf( super_prototype );
+
+  return function(){
+    try{
+      var result = super_prototype[ name ].apply( context, arguments );
+    }
+    catch(e){
+      if ( root ) delete self.stack[ name ];
+      throw e;
+    }
+    finally{
+      if ( root ) delete self.stack[ name ];
+    }
+
+    return result;
+  }
+};
+
+
+InheritingProxyHandler.prototype.get_prototype_with_method = function ( prototype, method ) {
+  while ( !prototype.hasOwnProperty( method ) ) {
+    prototype = Object.getPrototypeOf( prototype );
+    if ( prototype === null )
+      throw new TypeError( method + ' is not a function' );
+  }
+
+  return prototype;
+};
