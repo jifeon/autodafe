02fdd49e59a26820be418f08b31ef3074cda0ed7
Andrey Balakirev <balakirev.andrey@gmail.com>
2012-01-04 09:02:48 +0400

    tests for component.js

--- tests/units/components/component_tests.js
+++ tests/units/components/component_tests.js
@@ -1,33 +1,44 @@
-exports.get_batch = function( application, assert ) {
-  var Component = global.autodafe.Component;
+var vows        = require( 'autodafe/node_modules/vows' );
+var assert      = require( 'assert' );
+var tests_tools = require( 'autodafe/tests/tools/tests_tools' );
 
-  return {
-    topic : application,
+vows.describe( 'component' ).addBatch({
+
+  'After creating application' : {
+    topic : function(){
+      tests_tools.create_normal_application( this.callback );
+    },
+
     'wrong component creation without name' : function( app ) {
       assert.throws( function() {
-        new Component({
+        new autodafe.Component({
           app : app
         });
       } );
     },
-    'normal component -' : {
+
+    'and component' : {
       topic : function( app ) {
-        return new Component({
+        return new autodafe.Component({
           name : 'test',
           app  : app
         });
       },
+
       'property `name` should be' : {
+
         'equal "test"' : function( component ){
           assert.equal( component.name, 'test' );
         },
+
         'read only' : function( component ){
           assert.isReadOnly( component, 'name' );
         }
       },
-      '`get` method' : function( component ){
+
+      '`get` method should return himself' : function( component ){
         assert.equal( component.get(), component );
       }
     }
   }
-}
+}).export( module );
