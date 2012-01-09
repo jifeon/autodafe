65bcd2fd9fb4a3778f0ffef0825e0790aea0939a
Andrey Balakirev <balakirev.andrey@gmail.com>
2012-01-09 21:59:45 +0400

    #7: docs for router.js

--- framework/base/routing/router.js
+++ framework/base/routing/router.js
@@ -8,23 +8,119 @@ var qs          = require('querystring');
 
 module.exports = Router.inherits( AppModule );
 
+
+/**
+ * Router занимается маршрутизацией запросов и созданием URL
+ *
+ * Router настраивается в секции router конфигурационного файла. Обратиться к роутеру приложения можно через
+ * {@link Application.router}
+ *
+ * @constructor
+ * @extends AppModule
+ * @param {Object} params см. {@link Router._init}
+ * @example Настройка роутера конфигурационном файле
+ * <pre><code class="javascript">
+ * var config = module.exports = {
+ *   base_dir        : __dirname,
+ *   name            : 'AppName',
+ *
+ *   router : {
+ *     rules : {
+ *       // ...
+ *     }
+ *   }
+ * }
+ * </code></pre>
+ */
 function Router( params ) {
   this._init( params );
 }
 
 
+/**
+ * Инициализация Router
+ *
+ * @private
+ * @param {Object} params параметры для инициализации роутера, см. также {@link AppModule._init}
+ * @param {Object} [params.rules={}] правила маршрутизации запросов
+ *
+ * Ключи rules соответствуют части url адреса, которая идет после названия хоста и до параметров запроса в HTTP запросе
+ * и свойству action действия пришедшего по websockets. Внутри пути могут быть описаны параметры в виде
+ * <param_name:param_pattern> где param_name - имя параметра, а param_pattern - регулярное выражение соответствующее
+ * данному параметру. Также перед путем разделяя пробелами можно указать фильтры в виде filter_name:filter_value, где
+ * filter_name - часть распаршенного url, (см. <a href="http://nodejs.org/docs/latest/api/url.html#url.parse"url.parse</a>)
+ * а filter_value значение которое должно соответствовать части url. filter_name по умолчанию равно 'hostname'
+ *
+ * Значения rules - пути к действиям в виде 'контроллер.действие'. Если указать только контроллер: 'контроллер' - в
+ * качестве действия будет взято {@link Controller.default_action} этого контроллера, если указать пустую строку,
+ * будет взят {@link Application.default_controller}. После вертикально слэша можно указать методы и протоколы, по
+ * каоторым будет работать запрос: post, get, update, ws ( WebSockets )
+ *
+ * @example Таблица маршрутизации
+ * <pre><code class="javascript">
+ * router : {
+ *   rules : {
+ *     ''                    : 'site.index',
+ *     'posts'               : 'site.show_posts',
+ *     'post/<post_id:\\d+>' : 'site.show_post',
+ *     'delete_post'         : 'site.delete_post | post, delete',
+ *
+ *     'm.example.com /'                          : 'mobile.index',
+ *     'hostname:m.example.com port:3000 /posts'  : 'mobile.show_posts'
+ *   }
+ * }
+ * </code></pre>
+ *
+ */
 Router.prototype._init = function ( params ) {
   Router.parent._init.call( this, params );
 
+  /**
+   * Правила маршрутизации
+   *
+   * @private
+   * @type {Route[]}
+   */
   this._routes         = [];
+
+  /**
+   * Правила маршрутизации индексированные по пути к действию
+   *
+   * Используются для создания URL {@link Router.create_url}. Ключи - пути вида 'контроллер.действие', значения -
+   * массивы маршрутов {@link Route}
+   *
+   * @private
+   * @type {Object}
+   */
   this._routes_by_path = {};
+
+  /**
+   * Контроллеры
+   *
+   * Ключи - название контроллеров, значения - инициализиованные контроллеры {@link Controller}
+   *
+   * @private
+   * @type {Object}
+   */
   this._controllers    = {};
+
+  /**
+   * Регулярное выражение пути до действия контроллера
+   *
+   * Карманы: 3 - название контроллера или undefined, 4 - название действия или undefined
+   */
+  this._route_path_re  = /^(((\w+)\.)?(\w+))?$/;
 
   this._collect_controllers();
   this._parse_route_paths( params.rules );
 };
 
 
+/**
+ * Парсит пути
+ *
+ * @param {Object} rules пути, описание см. в {@link Router._init}
+ */
 Router.prototype._parse_route_paths = function ( rules ) {
   for ( var rule in rules ) {
     var route = new Route( {
@@ -37,11 +133,21 @@ Router.prototype._parse_route_paths = function ( rules ) {
     this._routes.push( route );
     var route_path = route.path;
     if ( !this._routes_by_path[ route_path ] ) this._routes_by_path[ route_path ] = [];
-    this._routes_by_path[ route.path ].push( route );
+    this._routes_by_path[ route_path ].push( route );
   }
 };
 
 
+/**
+ * Собирает и инициализирует контроллеры
+ *
+ * Контроллеры ищутся в {@link Application.path_to_controllers}
+ *
+ * @private
+ * @throws {Error} если возникнет проблема при подключении файла с контроллером
+ * @throws {Error} если возникнет проблема во время инициализации контроллера
+ *
+ */
 Router.prototype._collect_controllers = function () {
   var controllers_path = this.app.path_to_controllers;
   this.log( 'Collecting controllers in path: ' + controllers_path, 'trace' );
@@ -96,29 +202,38 @@ Router.prototype._collect_controllers = function () {
 };
 
 
-Router.prototype.throw_error = function ( message, number ) {
-  var error =  new Error( message );
-  error.number = number || 404;
-  throw error;
-};
-
-
+/**
+ * Перенаправляет запрос
+ *
+ * @param {Query} query запрос
+ * @throws {Error} 404 если не найден подходящий маршрут
+ * @throws {Error} 404 если не найден контроллер по наиболее релевантному маршруту
+ * @throws {Error} 404 если возникла ошибка при вызове действия контроллера ( см. {@link Controller.run_action})
+ */
 Router.prototype.route = function ( query ) {
+  var error;
 
   if ( !this._routes.some( function( route ){
     return route.is_suitable_for( query, true );
-  } ) )
-    this.throw_error( ('Route to `{route_rule}` failed. ' +
+  } ) ) {
+    error = new Error( ('Route to `{route_rule}` failed. ' +
       'File not found and route not specified in section router.rules of configuration file or ' +
       'specified for other protocol or query type than `{current_ct}`').format( {
         '{route_rule}' : query.action,
         '{current_ct}' : query.connection_type
       } )
     );
+    error.number = 404;
+    throw error;
+  }
 
   var route       = query.route;
   var controller  = this._controllers[ route.controller ];
-  if ( !controller ) this.throw_error( 'Controller "%s" is not found'.format( route.controller ) );
+  if ( !controller ) {
+    error = new Error( 'Controller "%s" is not found'.format( route.controller ) );
+    error.number = 404;
+    throw error;
+  }
 
   this.log( 'Route to `%s`'.format( route.path ), 'trace' );
 
@@ -132,12 +247,24 @@ Router.prototype.route = function ( query ) {
 };
 
 
+/**
+ * Создавет URL пользуясь таблицей маршрутизации
+ *
+ * Для создания URL из контроллера удобнее пользоваться функцией {@link Controller.create_url}
+ *
+ * @param {String} route_path путь к действию вида 'контроллер.действие', 'действие' или ''
+ * @param {Object} [params={}] параметры для формирования URL
+ * @param {String} [default_controller={@link Application.default_controller}] контроллер, который будет использоваться
+ * если он не указан в пути
+ * @param {String} [default_action=''] действие, которое будет использоваться если оно не указано в пути
+ * @returns {String} URL
+ */
 Router.prototype.create_url = function ( route_path, params, default_controller, default_action ) {
-  var matches = /^(((\w+)\.)?(\w+))?$/.exec( route_path );
+  var matches = this._route_path_re.exec( route_path );
   if ( !matches ) return route_path || '#';
 
-  var controller_name = matches[3] || default_controller;
-  var action_name     = matches[4] || default_action;
+  var controller_name = matches[3] || default_controller || this.app.default_controller;
+  var action_name     = matches[4] || default_action     || '';
   route_path          = controller_name + '.' + action_name;
 
   var routes  = this._routes_by_path[ route_path ];
@@ -148,7 +275,7 @@ Router.prototype.create_url = function ( route_path, params, default_controller,
   } ).sort( function( a, b ){
     return Object.keys( a.rule_params ).length < Object.keys( b.rule_params ).length;
   } )[0];
-  if ( !route ) return '';
+  if ( !route ) return '#';
 
   var rule  = route.get_rule( params );
   var query = qs.stringify( params );
@@ -157,6 +284,12 @@ Router.prototype.create_url = function ( route_path, params, default_controller,
 };
 
 
+/**
+ * Возвращает контроллер по имени
+ *
+ * @param {String} name имя контроллера
+ * @returns {Controller}
+ */
 Router.prototype.get_controller = function ( name ) {
   return this._controllers[ name ] || null;
 };
