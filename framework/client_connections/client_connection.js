510306635727c0a72332c3fef3bfb7b8f6c257e3
Andrey Balakirev <balakirev.andrey@gmail.com>
2012-01-31 11:04:08 +0400

    docs for client_connection.js

--- framework/client_connections/client_connection.js
+++ framework/client_connections/client_connection.js
@@ -1,27 +1,41 @@
-var Component = global.autodafe.Component;
-var http      = require( 'http' );
+var http = require( 'http' );
 
-module.exports = ClientConnection.inherits( Component );
+module.exports = ClientConnection.inherits( autodafe.Component );
 
+/**
+ * Базовый класс для компонентов выполняющих подключение к приложению
+ *
+ * @constructor
+ * @extends Component
+ * @param {Object} params см. {@link ClientConnection._init}
+ * @see HTTPServer
+ * @see WebSocketsServer
+ */
 function ClientConnection( params ) {
   this._init( params );
 }
 
 
+/**
+ * Инициализация подключения
+ *
+ * @private
+ * @param {Object} params параметров для инициализации подключения нет, см. {@link Component._init}
+ */
 ClientConnection.prototype._init = function ( params ) {
   ClientConnection.parent._init.call( this, params );
 
-  var self = this;
-  this.app.on( 'run', function() {
-    self.run();
-  } );
-  
-  this.app.on( 'close', function() {
-    self.close();
-  } );
+  this.app.on( 'run',   this.run.bind( this ) );
+  this.app.on( 'close', this.close.bind( this ) );
 };
 
 
+/**
+ * Создавет HTTP сервер и начинает слушать его на указанном порту
+ *
+ * @param {Number} port порт
+ * @returns {http.Server|null} Сервер, если все хорошо, и null, если порт занят
+ */
 ClientConnection.prototype.get_server = function ( port ) {
   var server = http.createServer();
 
@@ -37,5 +51,18 @@ ClientConnection.prototype.get_server = function ( port ) {
 };
 
 
+/**
+ * Выполняется при запуске приложения
+ *
+ * Метож можно перегрузить в наследуемых классах, и проводить в нем инициализацию, которая пройдет в полностью
+ * рабочем приложении
+ */
 ClientConnection.prototype.run    = function () {};
+
+
+/**
+ * Выполняется при закрытии приложения
+ *
+ * Метож можно перегрузить в наследуемых классах
+ */
 ClientConnection.prototype.close  = function () {};
