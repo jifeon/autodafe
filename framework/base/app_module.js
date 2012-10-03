6ee7574fb34c16e03fb3b04c48426bca1e8710ae
Andrey Balakirev <balakirev.andrey@gmail.com>
2012-10-04 01:41:11 +0400

    AppModule docs improved

--- framework/base/app_module.js
+++ framework/base/app_module.js
@@ -1,15 +1,18 @@
-module.exports = AppModule.inherits( autodafe.AutodafePart );
+module.exports = AppModule.inherits( global.autodafe.AutodafePart );
 
 /**
- * Базовый класс для модулей приложения. Поддерживает удобное логироние.
+ * Базовый класс для модулей приложения.
+ *
+ * Обеспечивает наличие ссылки на приложение во всех классах, унаследованных от него. А также поддерживает удобное
+ * логирование.
  *
  * @constructor
  * @extends AutodafePart
- * @param {Object} params см. {@link AppModule._init}
- * @example Пример использования
- *
- * <pre><code class="javascript">
- * module.exports = MyClass.inherits( autodafe.AppModule );
+ * @param {Object} params параметры для инициализации
+ * @param {Application} params.app ссылка на приложение в котором раобтает данный модуль
+ * @throws {Error} параметр params.app не является экземпляром {@link Application}
+ * @example <pre><code class="javascript">
+ * module.exports = MyClass.inherits( global.autodafe.AppModule );
  *
  * function MyClass( params ) {
  *   this._init( params );
@@ -17,15 +20,16 @@ module.exports = AppModule.inherits( autodafe.AutodafePart );
  *
  *
  * MyClass.prototype._init = function( params ) {
- *   MyClass.parent._init.call( this, params );   // вызываем родительский метод
+ *   // вызываем родительский метод
+ *   MyClass.parent._init.call( this, params );
  *
  *   //...
  * }
  *
- * //...
- *
+ * // использование
  * var instance = new MyClass({
- *   app : application,                           // обязательно передаем ссылку на приложение
+ *   // обязательно передаем ссылку на приложение
+ *   app : application,
  *   ...
  * });
  * </code></pre>
@@ -38,10 +42,8 @@ function AppModule( params ) {
 /**
  * Инициализация AppModule
  *
+ * @param {Object} params см. {@link AppModule}
  * @private
- * @param {Object} params параметры необходимые для инициализации
- * @param {Application} params.app ссылка на приложение в котором раобтает данный модуль
- * @throws {Error} если params.app не является {@link Application}
  */
 AppModule.prototype._init = function( params ) {
   AppModule.parent._init.call( this, params );
@@ -60,9 +62,9 @@ AppModule.prototype._init = function( params ) {
   /**
    * Ссылка на приложение
    *
-   * Содержится в лююом классе унаследованном от {@link AppModule}, подобных классов в фреймворке большинство.
+   * Содержится в любом классе унаследованном от {@link AppModule}, подобных классов в фреймворке большинство.
    * В теле методов контроллеров ({@link Controller}), моделей ({@link Model}), компонентов ({@link Component}) и т.д.
-   * можно смело использовать this.app для обращения к текущему приложению.
+   * можно смело использовать <code>this.app</code> для обращения к текущему приложению.
    *
    * @type {Application}
    */
@@ -82,16 +84,15 @@ AppModule.prototype._init = function( params ) {
  * Метод иcпользует {@link Logger.log}
  *
  * @param {String|Error} message Сообщение, которое будет залогировано. Может быть строкой содержащей текст сообщения
- * или ошибкой. В случае, когда тип параметра Error, второй параметр автоматически выставится в 'error', но его можно
- * переопределить на любой другой. Если в логгер попадает объект класса Error, то логгер показывает trace пользуясь
- * именно ошибкой ( Error.trace ) , что точнее определяет место ее возникновения
- * @param {String} [level] Тип сообщения, может быть "info", "trace", "warning" или "error". По умолчанию - "trace".
- * Если первый параметр ошибка, по умолчанию - "error"
- * @example Пример использования
- *
+ * или ошибкой. В случае, когда тип параметра <code>Error</code>, второй параметр автоматически выставится в
+ * <code>'error'</code>, но его можно переопределить на любой другой. Если в логгер попадает объект класса Error, то
+ * логгер показывает stack trace, пользуясь именно ошибкой (<code>Error.trace</code>). Это точнее определяет место ее
+ * возникновения
+ * @param {String} [level] Тип сообщения, может быть <code>info</code>, <code>trace</code>, <code>warning</code> или
+ * <code>error</code>. По умолчанию равен <code>trace</code>, а если первый параметр ошибка, по умолчанию -
+ * <code>error</code>
+ * @example Где-то в методе класса унаследованного от {@link AppModule}
  * <pre><code class="javascript">
- * // где-то в методе класса унаследованного от AppModule..
- *
  * this.log( 'Text of message', 'info' );
  * this.log( new Error( 'Error while doing something' ) );
  * this.log( new Error( 'Something wrong' ), 'warning' );
@@ -108,16 +109,14 @@ AppModule.prototype.log = function ( message, level ) {
  * Функция используется в местах где параметр callback необязателен, всместо него используется эта функция. Если первым
  * аргументом она получает не null и не undefined, она кидает ( throw ) этот параметр.
  *
- * @param [e] Чаще всего это ошибка, о которой нужно сообщить
- * @throws e - если он передан
- * @example Пример использования
- *
- * Рассмотрим метод, который делает какие-либо асинхронные действия и возвращает результат в callback, который в свою
- * очередь необязателен
- *
+ * @param {Error} [e=null] Ошибка, возникшая во время выполнения функции
+ * @throws {Error} параметр <code>e</code> отличен от <code>null</code>
+ * @example Рассмотрим метод, который делает какие-либо асинхронные действия и возвращает результат в callback, который
+ * в свою очередь необязателен:
  * <pre><code class="javascript">
  * MyClass.prototype.do_something_async = function( callback ) {
- *   // проверяем полученный параметр и заменяем его на стандартный если callback не передан или передана не функция
+ *   // проверяем полученный параметр и заменяем его на стандартный callback, если callback не передан или переданный
+ *   // параметр не является функцией
  *   callback = typeof callback == 'function' ? callback : this.default_callback;
  *
  *   // ...что-нибудь делаем
@@ -127,8 +126,7 @@ AppModule.prototype.log = function ( message, level ) {
  *   return callback( null, result );
  * }
  * </code></pre>
- *
- * Теперь даже если метод не получит callback, ошибка не замолчится а пойдет дальше по указанным правилам.
+ * Теперь даже если метод не получит callback, ошибка не замолчится, а пойдет дальше по указанным правилам.
  */
 AppModule.prototype.default_callback = function ( e ) {
   if ( e != null ) throw e;
