1587e2938484e84fc0602ae83c827370006efb33
jifeon <balakirev.andrey@gmail.com>
2014-01-26 11:45:06 +0400

    version 1.0 started

--- tests/createApplication.js
+++ tests/createApplication.js
@@ -1,2 +1,11 @@
 var vows = require('vows'),
     assert = require('assert');
+
+vows.describe('Autodafe').addBatch({
+    'test': {
+        topic: 42,
+        'should be 42': function (topic) {
+            assert.equal(topic, 42);
+        }
+    }
+}).export(module);
