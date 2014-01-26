7be609525ed47d5a2b70b0f50f75476d74ec0f61
jifeon <balakirev.andrey@gmail.com>
2014-01-27 00:42:52 +0400

    AtdClass support for inheritance

--- tests/lib_tests.js
+++ tests/lib_tests.js
@@ -30,6 +30,67 @@ vows.describe('library tests').addBatch({
             assert.instanceOf(myEx2, MyClass2);
             assert.instanceOf(myEx2, MyClass);
             assert.instanceOf(myEx2, AtdClass);
+        },
+        'should provide the _super property, which calls method of parent class': function () {
+            var parentMethodInvoked = false,
+                childMethodInvoked = false;
+
+            var MyClass = AtdClass.extend({
+                m: function () {
+                    parentMethodInvoked = true;
+                }
+            });
+
+            var MyClass2 = MyClass.extend({
+                m: function () {
+                    this._super();
+                    childMethodInvoked = true;
+                }
+            });
+
+            var myEx = new MyClass2;
+            myEx.m();
+
+            assert.isTrue(parentMethodInvoked);
+            assert.isTrue(childMethodInvoked);
+        },
+        'should invoke _prop and _init method while initialization': function () {
+            var propsInvoked = false,
+                initInvoked = false,
+                initInvokedBeforeProp;
+            var MyClass = AtdClass.extend({
+                _props: function () {
+                    this._super();
+
+                    propsInvoked = true;
+                    initInvokedBeforeProp = initInvoked;
+                },
+                _init: function () {
+                    this._super();
+
+                    initInvoked = true;
+                }
+            });
+            var myEx = new MyClass;
+            assert.isTrue(propsInvoked);
+            assert.isTrue(initInvoked);
+            assert.isFalse(initInvokedBeforeProp);
+        },
+        '_init and _props methods should contain a call to _super method': function () {
+            assert.throws(function () {
+                AtdClass.extend({
+                    _init: function () {
+
+                    }
+                });
+            });
+            assert.throws(function () {
+                AtdClass.extend({
+                    _props: function () {
+
+                    }
+                });
+            });
         }
     }
 }).export(module);
